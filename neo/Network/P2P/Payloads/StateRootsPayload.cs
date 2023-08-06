5a85ba3e86b3e3d1f9883684a3d572798b43c9f4
Charalampos Chomenidis <hampos@me.com>
2023-08-06 13:37:56 +0300

    Increase State Root message size to 500 for a slight increase on root sync process

--- neo/Network/P2P/Payloads/StateRootsPayload.cs
+++ neo/Network/P2P/Payloads/StateRootsPayload.cs
@@ -8,7 +8,7 @@ namespace Neo.Network.P2P.Payloads
 {
     public class StateRootsPayload : ISerializable
     {
-        public const int MaxStateRootsCount = 200;
+        public const int MaxStateRootsCount = 500;
 
         public StateRoot[] StateRoots;
 
