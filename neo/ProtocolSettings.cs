47aca557c2a81dec3b01c791107fd17de02b7e42
Charalampos Chomenidis <hampos@me.com>
2024-01-12 12:55:21 +0200

    Remove default Neo SeedList and StandbyValidators

--- neo/ProtocolSettings.cs
+++ neo/ProtocolSettings.cs
@@ -56,28 +56,12 @@ namespace Neo
             if (section_sv.Exists())
                 this.StandbyValidators = section_sv.GetChildren().Select(p => p.Get<string>()).ToArray();
             else
-                this.StandbyValidators = new[]
-                {
-                    "03b209fd4f53a7170ea4444e0cb0a6bb6a53c2bd016926989cf85f9b0fba17a70c",
-                    "02df48f60e8f3e01c48ff40b9b7f1310d7a8b2a193188befe1c2e3df740e895093",
-                    "03b8d9d5771d8f513aa0869b9cc8d50986403b78c6da36890638c3d46a5adce04a",
-                    "02ca0e27697b9c248f6f16e085fd0061e26f44da85b58ee835c110caa5ec3ba554",
-                    "024c7b7fb6c310fccf1ba33b082519d82964ea93868d676662d4a59ad548df0e7d",
-                    "02aaec38470f6aad0042c6e877cfd8087d2676b0f516fddd362801b9bd3936399e",
-                    "02486fd15702c4490a26703112a5cc1d0923fd697a33406bd5a1c00e0013b09a70"
-                };
+                throw new ArgumentException("StandbyValidators section in protocol is empty.");
             IConfigurationSection section_sl = section.GetSection("SeedList");
             if (section_sl.Exists())
                 this.SeedList = section_sl.GetChildren().Select(p => p.Get<string>()).ToArray();
             else
-                this.SeedList = new[]
-                {
-                    "seed1.neo.org:10333",
-                    "seed2.neo.org:10333",
-                    "seed3.neo.org:10333",
-                    "seed4.neo.org:10333",
-                    "seed5.neo.org:10333"
-                };
+                throw new ArgumentException("SeedList section in protocol is empty.");
             Dictionary<TransactionType, Fixed8> sys_fee = new Dictionary<TransactionType, Fixed8>
             {
                 [TransactionType.EnrollmentTransaction] = Fixed8.FromDecimal(1000),
